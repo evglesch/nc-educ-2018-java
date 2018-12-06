5f18a37a8e24464e0d0b2072ac9e6cebe2879841
evglesch <evlewrk@gmail.com>
2018-12-06 14:49:47 +0400

    Add PG empty examples

--- src/main/java/jdbc/JdbcApplication.java
+++ src/main/java/jdbc/JdbcApplication.java
@@ -4,15 +4,40 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 public class JdbcApplication {
 
     public static void main(String[] args) {
         Connection connection = DatabaseDriver.openPostgresConnection();
+    }
 
+    private static void selectExample() {
 
     }
+
+    private static void insertExample() {
+
+    }
+
+    private static void updateExample() {
+
+    }
+
+    private static void deleteExample() {
+
+    }
+
+    private static void autoCommitExample() {
+
+    }
+
+    private static void mapToObjectExample() {
+
+    }
+
+
 
 
 
